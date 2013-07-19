aa39e333d000f953402db0568021d24f43d12bee
Kaiyang.tee <kaiyang.tee@wustl.edu>
2013-07-19 11:48:07 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wewewdwedew::Application.config.secret_token = 'ea3eb3d618245c4bccc547010787430088d1d49857581598e124ac589e8408ef1831fc96a0aef4816ef66805b5383428601667681ccd7f76137cb96cf5702793'
